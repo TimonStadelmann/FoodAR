b9986d44456f87cd99f9640645bb789db2b4586e
Timon Stadelmann <stadelmann.timon@gmail.com>
2024-12-02 17:00:11 +0100

    setup

--- src/js/index.js
+++ src/js/index.js
@@ -4,28 +4,28 @@ import { createScene } from './scene';
 import { browserHasImmersiveArCompatibility, displayIntroductionMessage, displayUnsupportedBrowserMessage } from './utils/domUtils';
 
 function initializeXRApp() {
-	const { devicePixelRatio, innerHeight, innerWidth } = window;
+    const { devicePixelRatio, innerHeight, innerWidth } = window;
 
-	const renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 
-	renderer.setSize(innerWidth, innerHeight);
-	renderer.setPixelRatio(devicePixelRatio);
+    renderer.setSize(innerWidth, innerHeight);
+    renderer.setPixelRatio(devicePixelRatio);
 
-	renderer.xr.enabled = true;
+    renderer.xr.enabled = true;
 
-	document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
-	document.body.appendChild(ARButton.createButton(renderer, { requiredFeatures: ['hit-test'] }));
+    document.body.appendChild(ARButton.createButton(renderer, { requiredFeatures: ['hit-test'] }));
 
-	displayIntroductionMessage();
+    displayIntroductionMessage();
 
-	createScene(renderer);
+    createScene(renderer);
 }
 
 async function start() {
-	const isImmersiveArSupported = await browserHasImmersiveArCompatibility();
+    const isImmersiveArSupported = await browserHasImmersiveArCompatibility();
 
-	isImmersiveArSupported ? initializeXRApp() : displayUnsupportedBrowserMessage();
+    isImmersiveArSupported ? initializeXRApp() : displayUnsupportedBrowserMessage();
 }
 
 start();
