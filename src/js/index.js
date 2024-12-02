6610a11dd8ea27b7d1ecb3cbc9ec62982488dde1
Timon Stadelmann <stadelmann.timon@gmail.com>
2024-12-02 14:25:06 +0100

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Document Loaded
-});
+import { WebGLRenderer } from 'three/src/renderers/WebGLRenderer';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton';
+import { createScene } from './scene';
+import { browserHasImmersiveArCompatibility, displayIntroductionMessage, displayUnsupportedBrowserMessage } from './utils/domUtils';
+
+function initializeXRApp() {
+	const { devicePixelRatio, innerHeight, innerWidth } = window;
+
+	const renderer = new WebGLRenderer({ antialias: true, alpha: true });
+
+	renderer.setSize(innerWidth, innerHeight);
+	renderer.setPixelRatio(devicePixelRatio);
+
+	renderer.xr.enabled = true;
+
+	document.body.appendChild(renderer.domElement);
+
+	document.body.appendChild(ARButton.createButton(renderer, { requiredFeatures: ['hit-test'] }));
+
+	displayIntroductionMessage();
+
+	createScene(renderer);
+}
+
+async function start() {
+	const isImmersiveArSupported = await browserHasImmersiveArCompatibility();
+
+	isImmersiveArSupported ? initializeXRApp() : displayUnsupportedBrowserMessage();
+}
+
+start();
